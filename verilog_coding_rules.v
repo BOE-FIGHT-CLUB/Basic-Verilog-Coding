3ff2e21206318fd56413001e53df7301c15d2c05
BigKuchz <ck859613529@gmail.com>
2020-12-02 21:57:05 +0800

    no commit

--- verilog_coding_rules.v
+++ verilog_coding_rules.v
@@ -2,10 +2,15 @@
 //
 //  Verilog Tamplate Module
 //
+// sync with https://github.com/BigKuchz/Basic-Verilog-Coding.git
+//
+//
 //========================================
 
 module boe_fight_club_top(
 
 );
+
+
 
 endmodule
